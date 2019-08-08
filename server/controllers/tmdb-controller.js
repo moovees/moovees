5245da6e9c0ec374a11a0b5314eb3b9ece572f78
havus <havus.it@gmail.com>
2019-08-08 20:13:25 +0700

    get detail movie

--- server/controllers/tmdb-controller.js
+++ server/controllers/tmdb-controller.js
@@ -1,5 +1,22 @@
-class TmdbController {
+const axios = require('axios');
+const params = {
+  api_key: process.env.TMDB_API_KEY,
+  language: 'en-US',
+  region: 'indonesia'
+}
 
+class TmdbController {
+  static getDetails(req, res, next) {
+    axios.get(`https://api.themoviedb.org/3/movie/${req.params.movie_id}`, { params })
+    .then((response) => {
+      res.send(response.data)
+    })
+    .catch((err) => {
+      res.send(err);
+    })
+  }
+
+  
 }
 
 module.exports = TmdbController
--- server/routes/tmdb-route.js
+++ server/routes/tmdb-route.js
@@ -1,8 +1,9 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
-const TmdbController = require('../controllers/tmdb-controller')
-
+const TmdbController = require('../controllers/tmdb-controller');
+// tmdb/popular
+router.get('/:movie_id', TmdbController.getDetails);
 
 
 module.exports = router
