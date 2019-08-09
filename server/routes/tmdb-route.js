2e864adb1de5da288951d1fc7e8c39428379bfa7
adityabpradana <adityabpradana@gmail.com>
2019-08-09 14:16:37 +0700

    add recommendation

--- server/routes/tmdb-route.js
+++ server/routes/tmdb-route.js
@@ -7,5 +7,6 @@ router.get('/:movie_id', TmdbController.getDetails);
 router.get('/:movie_id/poster', TmdbController.getPoster);
 router.get('/popular/:page', TmdbController.getPopular);
 router.get('/top_rated/:page', TmdbController.getTopRated);
+router.get('/search/movie', TmdbController.searchMovie)
 
 module.exports = router
